5bd9ea4f24a1b9ba9eb80a6e907005493d30b13f
Ishaya Solomon <ishaya808@gmail.com>
2023-03-11 14:30:22 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-ixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fix PHP file typo mistake in a LAMP wordpress setup
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec {'correct typo':
+  provider => shell,
+  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
 }
